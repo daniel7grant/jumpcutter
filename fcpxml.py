ec30b9c667a0a64d830f74b0f220fcc00aef479b
Grant <gergely.daniel7@gmail.com>
2022-08-19 23:13:51 +0200

    Create XML file with simple cutter

---
+++ fcpxml.py
@@ -0,0 +1,171 @@
+from os import path
+
+
+class FcpXmlBuilder:
+    def __init__(self, sourceFile, dimensions, frameRate, duration):
+        self.sourceFile = sourceFile
+        self.name = path.basename(sourceFile)
+        (width, height) = dimensions
+        self.width = width
+        self.height = height
+        self.frameRate = frameRate
+        self.duration = duration
+        self.index = 0
+        self.frameIndex = 0
+        self.videoClips = []
+        self.audioClips = []
+
+    def addClip(self, start, end):
+        self.addVideoClip(start, end)
+        self.addAudioClip(start, end)
+        self.index += 1
+        self.frameIndex += end - start
+
+    def addVideoClip(self, start, end):
+        self.videoClips.append(
+            f'''
+                    <clipitem id="{self.name} {self.index}">
+                        <name>{self.name}</name>
+                        <duration>{self.duration}</duration>
+                        <rate>
+                            <timebase>{self.duration}</timebase>
+                            <ntsc>FALSE</ntsc>
+                        </rate>
+                        <start>{self.frameIndex}</start>
+                        <end>{self.frameIndex + (end - start)}</end>
+                        <enabled>TRUE</enabled>
+                        <in>{start}</in>
+                        <out>{end}</out>
+                        <file id="{self.name} {self.index}">
+                            <duration>{self.duration}</duration>
+                            <rate>
+                                <timebase>{self.frameRate}</timebase>
+                                <ntsc>FALSE</ntsc>
+                            </rate>
+                            <name>{self.name}</name>
+                            <pathurl>file://{self.sourceFile}</pathurl>
+                            <timecode>
+                                <string>00:00:00:00</string>
+                                <displayformat>NDF</displayformat>
+                                <rate>
+                                    <timebase>{self.frameRate}</timebase>
+                                    <ntsc>FALSE</ntsc>
+                                </rate>
+                            </timecode>
+                            <media>
+                                <video>
+                                    <duration>{self.duration}</duration>
+                                    <samplecharacteristics>
+                                        <width>{self.width}</width>
+                                        <height>{self.height}</height>
+                                    </samplecharacteristics>
+                                </video>
+                                <audio>
+                                    <channelcount>2</channelcount>
+                                </audio>
+                            </media>
+                        </file>
+                        <compositemode>normal</compositemode>
+                    </clipitem>
+            '''
+        )
+
+    def addAudioClip(self, start, end):
+        self.audioClips.append(
+            f'''
+                    <clipitem id="{self.name} {self.index}">
+                        <name>{self.name}</name>
+                        <duration>{self.duration}</duration>
+                        <rate>
+                            <timebase>{self.frameRate}</timebase>
+                            <ntsc>FALSE</ntsc>
+                        </rate>
+                        <start>{self.frameIndex}</start>
+                        <end>{self.frameIndex + (end - start)}</end>
+                        <enabled>TRUE</enabled>
+                        <in>{start}</in>
+                        <out>{end}</out>
+                        <file id="{self.name} {self.index}">
+                            <duration>{self.duration}</duration>
+                            <rate>
+                                <timebase>{self.frameRate}</timebase>
+                                <ntsc>FALSE</ntsc>
+                            </rate>
+                            <name>{self.name}</name>
+                            <pathurl>file://{self.sourceFile}</pathurl>
+                            <media>
+                                <audio>
+                                    <channelcount>2</channelcount>
+                                </audio>
+                            </media>
+                        </file>
+                        <sourcetrack>
+                            <mediatype>audio</mediatype>
+                            <trackindex>1</trackindex>
+                        </sourcetrack>
+                        <comments/>
+                    </clipitem>
+            '''
+        )
+
+    def dump(self):
+        return f'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE xmeml>
+<xmeml version="5">
+    <sequence>
+        <name>Timeline {self.name}</name>
+        <duration>{self.duration}</duration>
+        <rate>
+            <timebase>{self.frameRate}</timebase>
+            <ntsc>FALSE</ntsc>
+        </rate>
+        <in>-1</in>
+        <out>-1</out>
+        <timecode>
+            <string>01:00:00:00</string>
+            <frame>108000</frame>
+            <displayformat>NDF</displayformat>
+            <rate>
+                <timebase>{self.frameRate}</timebase>
+                <ntsc>FALSE</ntsc>
+            </rate>
+        </timecode>
+        <media>
+            <video>
+                <track>
+                    {" ".join(self.videoClips)}
+                    <enabled>TRUE</enabled>
+                    <locked>FALSE</locked>
+                </track>
+                <format>
+                    <samplecharacteristics>
+                        <width>{self.width}</width>
+                        <height>{self.height}</height>
+                        <pixelaspectratio>square</pixelaspectratio>
+                        <rate>
+                            <timebase>{self.frameRate}</timebase>
+                            <ntsc>FALSE</ntsc>
+                        </rate>
+                        <codec>
+                            <appspecificdata>
+                                <appname>Final Cut Pro</appname>
+                                <appmanufacturer>Apple Inc.</appmanufacturer>
+                                <data>
+                                    <qtcodec/>
+                                </data>
+                            </appspecificdata>
+                        </codec>
+                    </samplecharacteristics>
+                </format>
+            </video>
+            <audio>
+                <track>
+                    {" ".join(self.audioClips)}
+                    <enabled>TRUE</enabled>
+                    <locked>FALSE</locked>
+                </track>
+            </audio>
+        </media>
+    </sequence>
+</xmeml>
+'''
--- simplecutter.py
+++ simplecutter.py
@@ -1,61 +1,111 @@
 import argparse
 import json
 import numpy as np
+import re
 import subprocess
+from fcpxml import FcpXmlBuilder
 from functools import reduce
+from os import path
 from scipy.io import wavfile
 
 
 def getMaxVolume(s):
     maxv = float(np.max(s))
     minv = float(np.min(s))
-    return max(maxv,-minv)
+    return max(maxv, -minv)
+
 
 def groupTuples(xs, n):
     return list(map(
-        lambda i: xs[i : min(i + n, len(xs))],
+        lambda i: xs[i: min(i + n, len(xs))],
         range(len(xs))
     ))
 
 
+def inputToXmlFilename(filename):
+    dotIndex = filename.rfind(".")
+    return filename[:dotIndex]+".xml"
+
+
 # Handle parameters and setup values
-parser = argparse.ArgumentParser(description='Modifies a video file to play at different speeds when there is sound vs. silence.')
-parser.add_argument('--input_file', type=str,  help='the video file you want modified')
-parser.add_argument('--url', type=str, help='A youtube url to download and process')
-parser.add_argument('--output_file', type=str, default="", help="the output file. (optional. if not included, it'll just be the input file name)")
-parser.add_argument('--output_path', type=str, default="", help="the location where it should put the output files. (by default: local directory)")
-parser.add_argument('--xml_file', type=str, default="", help="the XML timeline output. (default: the input file's name + .xml)")
-parser.add_argument('--silent_threshold', type=float, default=0.03, help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
-parser.add_argument('--frame_margin', type=int, default=1, help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
-parser.add_argument('--sample_rate', type=float, default=44100, help="sample rate of the input and output videos")
-parser.add_argument('--frame_rate', type=float, default=30, help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
-parser.add_argument('--frame_quality', type=int, default=3, help="quality of frames to be extracted from input video. 1 is highest, 31 is lowest, 3 is the default.")
+parser = argparse.ArgumentParser(
+    description='Modifies a video file to play at different speeds when there is sound vs. silence.')
+parser.add_argument('--input_file', type=str,
+                    help='the video file you want modified')
+parser.add_argument('--xml_file', type=str, default="",
+                    help="the XML timeline output. (default: the input file's name + .xml)")
+parser.add_argument('--silent_threshold', type=float, default=0.03,
+                    help="the volume amount that frames' audio needs to surpass to be consider \"sounded\". It ranges from 0 (silence) to 1 (max volume)")
+parser.add_argument('--frame_margin', type=int, default=1,
+                    help="some silent frames adjacent to sounded frames are included to provide context. How many frames on either the side of speech should be included? That's this variable.")
+parser.add_argument('--sample_rate', type=float, default=44100,
+                    help="sample rate of the input and output videos")
+parser.add_argument('--frame_rate', type=float, default=30,
+                    help="frame rate of the input and output videos. optional... I try to find it out myself, but it doesn't always work.")
 
 args = parser.parse_args()
 
+# Setup variables
 SILENT_THRESHOLD = args.silent_threshold
 SAMPLE_RATE = args.sample_rate
 FRAME_RATE = args.frame_rate
 FRAME_MARGIN = args.frame_margin
+DIMENSIONS = (1920, 1080)  # probably
 
-INPUT_FILE = args.input_file
-assert INPUT_FILE != None , "you have to add an input file"
-    
-if len(args.output_file) >= 1:
-    OUTPUT_FILE = args.output_file
+INPUT_FILE = path.abspath(args.input_file)
+assert INPUT_FILE != None, "you have to add an input file"
+
+if len(args.xml_file) >= 1:
+    XML_FILE = path.abspath(args.xml_file)
 else:
-    OUTPUT_FILE = INPUT_FILE
+    XML_FILE = inputToXmlFilename(INPUT_FILE)
+
+# Get video metadata
+# metadata = subprocess.run(["ffprobe", INPUT_FILE], capture_output=True).stderr.decode("utf-8")
+# frameRateMatches = re.search(r'([0-9]+) fps', metadata)
+# if frameRateMatches is not None:
+#     FRAME_RATE = int(frameRateMatches.group(1))
+
+# sampleRateMatches = re.search(r'([0-9]+) Hz', metadata)
+# if sampleRateMatches is not None:
+#     SAMPLE_RATE = int(sampleRateMatches.group(1))
+
+# dimensionMatches = re.search(r'([0-9]+)x([0-9]+)', metadata)
+# if dimensionMatches is not None:
+#     DIMENSIONS = (int(dimensionMatches.group(1)), int(dimensionMatches.group(2)))
+
+frames = subprocess.run([
+    "ffprobe",
+    "-v", "error",
+    "-select_streams", "v:0",
+    "-count_packets",
+    "-show_entries", "stream=nb_read_packets",
+    "-of", "csv=p=0",
+    INPUT_FILE
+], capture_output=True).stdout.decode("utf-8")
+DURATION = int(frames)
 
 # Generate wav file to calculate peaks
-command = "ffmpeg -y -i "+INPUT_FILE+" -ab 160k -ac 2 -ar "+str(SAMPLE_RATE)+" -vn audio.wav"
-subprocess.call(command, shell=True)
+subprocess.run([
+    "ffmpeg",
+    "-y",
+    "-i", INPUT_FILE,
+    "-ab", "160k",
+    "-ac", "2",
+    "-ar", f"{SAMPLE_RATE}",
+    "-vn", "audio.wav",
+], capture_output=True)
 sampleRate, audioData = wavfile.read("audio.wav")
 maxAudioVolume = getMaxVolume(audioData)
 
 # Generate list of bool values for silent (False) and loud (True) parts
-groupedAudioData = np.array_split(audioData, int(sampleRate / FRAME_RATE))
+seconds = int(audioData.shape[0] / sampleRate * FRAME_RATE)
+groupedAudioData = np.array_split(audioData, seconds)
 avgAudioData = map(getMaxVolume, groupedAudioData)
-loudAudioData = list(map(lambda x: x > maxAudioVolume * SILENT_THRESHOLD, avgAudioData))
+loudAudioData = list(map(
+    lambda x: x > maxAudioVolume * SILENT_THRESHOLD,
+    avgAudioData
+))
 fuzzyLoudAudioData = list(map(any, groupTuples(loudAudioData, FRAME_MARGIN)))
 
 # Find the start and end of loud clips
@@ -75,7 +125,13 @@ if fuzzyLoudAudioData[0]:
 
 # Handle when the end is loud
 if fuzzyLoudAudioData[-1]:
-    ends.append(len(fuzzyLoudAudioData))
+    ends.append(len(fuzzyLoudAudioData) - 1)
 
-# Print starts and ends
-print(list(zip(starts, ends)))
+# Create XML clips for every start and end
+fcpxml = FcpXmlBuilder(INPUT_FILE, DIMENSIONS, FRAME_RATE, DURATION)
+for (start, end) in zip(starts, ends):
+    fcpxml.addClip(start, end)
+
+# Write output to XML file
+with open(XML_FILE, "w") as xml_file:
+    xml_file.write(fcpxml.dump())
